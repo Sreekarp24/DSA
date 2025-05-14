803cf39fda872934461d0907abdd55bf116a56df
Sreekarp24 <sreekarpagadala@gmail.com>
2025-05-14 07:46:25 +0530

    Even Odd Number

---
+++ evenodd.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num;
+    cin >> num;
+    if(num % 2 ==0){    
+    cout << "It is an Even Number";
+}
+else{
+    cout << "It is an Odd Number";
+}
+return 0;
+}
